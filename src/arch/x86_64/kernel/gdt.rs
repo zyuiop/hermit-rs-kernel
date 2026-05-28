108a0346b46c4b5ddbb03538e736acf7ccce628f
Louis Vialar <louis.vialar@gmail.com>
2026-05-28 22:54:08 +0200

    fix: increase stack size for interrupts handling.......

--- src/arch/x86_64/kernel/gdt.rs
+++ src/arch/x86_64/kernel/gdt.rs
@@ -43,14 +43,14 @@ pub fn add_current_core() {
 	// Allocate all ISTs for this core.
 	// Every task later gets its own IST, so the IST allocated here is only used by the Idle task.
 	for i in 0..IST_ENTRIES {
-		let size = if i < 2 {
-			IST_SIZE
-		} else {
-			BasePageSize::SIZE as usize
-		};
+		let size = IST_SIZE;
 
 		let layout = Layout::from_size_align(size, BasePageSize::SIZE as usize).unwrap();
 		let ist = unsafe { alloc(layout) };
+
+		// TODO: we should put together some stack overflow protection here.
+		// TODO: suggestion: allocate dedicated frames, and unmap the first frame of the stack without putting it back in the available addresses pool
+
 		assert!(!ist.is_null());
 		let ist_start = unsafe { ist.add(size - TaskStacks::MARKER_SIZE) };
 		tss.interrupt_stack_table[i] = VirtAddr::from_ptr(ist_start);
